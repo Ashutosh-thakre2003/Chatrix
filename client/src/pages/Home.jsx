b6c623ee04cb1883c146e374dc3d71b9bce7dd84
Ashutosh-thakre2003 <162476813+Ashutosh-thakre2003@users.noreply.github.com>
2025-05-26 13:32:33 +0530

    Delete client directory

--- client/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  const user = JSON.parse(localStorage.getItem('user') || '{}');
-  return (
-    <div>
-      <h1>Welcome, {user.username}</h1>
-      {user.avatar && <img src={user.avatar} alt="avatar" style={{ width: 80 }} />}
-    </div>
-  );
-}
